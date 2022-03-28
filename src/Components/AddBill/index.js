0720d180e8c67902dc645733cc65129bc97900c0
Scott Wilson <swrs@hotmail.co.uk>
2022-03-28 12:05:06 +0100

    created component folders and files and imported into app.js

---
+++ src/Components/AddBill/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddBill() {
+  return <div></div>;
+}
+
+export default AddBill;
---
+++ src/Components/SplitBillBox/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SplitBillBox() {
+  return <div></div>;
+}
+
+export default SplitBillBox;
