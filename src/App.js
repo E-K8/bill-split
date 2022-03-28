32b88cfb770aced8b98f5713254c326ec4f8f0a2
Scott Wilson <swrs@hotmail.co.uk>
2022-03-28 14:23:09 +0100

    created fetch request and passed data down to BillBox

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import AddBill from "./Components/AddBill";
 import BillBox from "./Components/BillBox";
 import SplitBillBox from "./Components/SplitBillBox";
+// import GetBills from "./Components/useFetch";
+import useFetch from "react-fetch-hook";
 
 function App() {
+  const { isLoading, data } = useFetch(
+    "https://billsplitters.herokuapp.com/bills"
+  );
   return (
     <div className="App">
       <h1>Split you bills</h1>
+      {/* <GetBills /> */}
       <AddBill />
-      <BillBox />
+      {data ? <BillBox data={data.payload} /> : <></>}
       <SplitBillBox />
     </div>
   );
--- src/Components/BillBox/index.js
+++ src/Components/BillBox/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Card } from "antd";
 import "antd/dist/antd.css";
 
-function BillBox() {
+function BillBox({ data }) {
+  console.log(data);
   return (
     <Card title="Card title">
       <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
